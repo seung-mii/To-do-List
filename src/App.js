0563beeb5162872596477ddb37341fda3494258c
withsme <tmdal9953@gmail.com>
2023-03-31 10:23:33 +0900

    3rd commit : 1st commit 내용을 function component로 변경

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Hello from './Hello';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <Hello />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className='App'>
+      <Hello />
+    </div>
+  )
 }
 
 export default App;
--- src/Hello.js
+++ src/Hello.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 
-class Hello extends React.Component {
-  render() {
-    return (
-      <div className='Hello'>
-        <label for="hello">안녕하세요!</label>
-      </div>
-    )
-  }
+function Hello() {
+  return (
+    <div className='Hello'>
+      <label for="hello">안녕하세요!</label>
+    </div>
+  )
 }
 
 export default Hello;
