3928da8d868c344443f9fa2123726743d462e80c
withsme <tmdal9953@gmail.com>
2023-03-31 09:43:18 +0900

    1st commit : Hello 컴포넌트를 로드하여 안녕하세요! 출력하기

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div className='Hello'>
+        <label for="hello">안녕하세요!</label>
+      </div>
+    )
+  }
+}
+
+export default Hello;
