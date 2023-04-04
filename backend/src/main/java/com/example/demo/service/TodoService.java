a29b658ac6ef2abdf64cd124dd5744d123c3c12e
withsme <tmdal9953@gmail.com>
2023-04-04 15:48:10 +0900

    14th commit : @NamedQuery, @Query 사용하기

--- backend/src/main/java/com/example/demo/service/TodoService.java
+++ backend/src/main/java/com/example/demo/service/TodoService.java
@@ -19,7 +19,9 @@ public class TodoService {
 		repository.save(entity);
 
 		// Todo Entity 검색
-		TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
+		// TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
+		// return savedEntity.getUserId();
+		TodoEntity savedEntity = repository.searchByUserId(entity.getUserId()).get(0);
 		return savedEntity.getUserId();
 	}
 }
