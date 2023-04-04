850b3424f60629ef8c69d11fcf8a40ed91fa15aa
withsme <tmdal9953@gmail.com>
2023-04-04 13:23:50 +0900

    11th commit : SpringBoot controller 어노테이션 사용하기

--- backend/src/main/java/com/example/demo/controller/TestController.java
+++ backend/src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,18 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("test")
@@ -10,5 +20,29 @@ public class TestController {
 	@GetMapping
 	public String testController() {
 		return "Hello World!(안녕하세요!)";
+	}
+
+	@GetMapping("/test/{id}")
+	public String testControllerWithPathVariable(@PathVariable(required = false) int id) {
+		return "Hello World! ID " + id;
+	}
+
+	@GetMapping("/Param")
+	public String testControllerRequestParam(@RequestParam(required = false) int id) {
+		return "Hello World! ID param " + id;
+	}
+
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! ID " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+	}
+
+	@GetMapping("/testResponseBody")
+	public ResponseDTO<String>testControllerResponseBody() {
+		List<String> list = new ArrayList<String>();
+		list.add("Hello World! I'm ResponseDTO");
+		list.add("See you!");
+		ResponseDTO<String>response = ResponseDTO.<String>builder().data(list).build();
+		return response;
 	}
 }
