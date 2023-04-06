3e399cc1138d042f74037db1da5a51a749925f97
withsme <tmdal9953@gmail.com>
2023-04-06 15:40:44 +0900

    23rd commit : 회원가입, 로그인, To-do 완성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Todo from './Todo';
 import AddTodo from './AddTodo';
-import { Paper, List, Container } from "@material-ui/core";
+import { Paper, List, Container, Grid, Button, AppBar, Toolbar, Typography } from "@material-ui/core";
 import './App.css';
-import { call } from './service/ApiService';
+import { call, signout } from './service/ApiService';
 
 class App extends React.Component {
   constructor(props) { // 매개변수 props 생성자 
     super(props);      // 매개변수 props 초기화
     this.state = {     // item에 item.id, item.title, item.done 매개변수 이름과 값 할당
       items: [],
+      // 로딩 중이라는 상태를 표현할 변수 생성자에 상태 변수를 초기화한다.
+      loading: true,
     };
   }
 
@@ -31,9 +33,10 @@ class App extends React.Component {
     );
   }
 
+  // componentDidMount 는 페이지(돔) 마운트가 일어나고 렌더링 되기 전에 실행된다.
   componentDidMount() {
     call("/todo", "GET", null).then((response) => 
-      this.setState({items:response.data})
+      this.setState({items:response.data, loading:false})
     );
   }
 
@@ -44,18 +47,53 @@ class App extends React.Component {
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
+            <Todo item={item} key={item.id} delete={this.delete} update={this.update} />
           ))}
         </List>
       </Paper>
     );
 
-    return (
-      <div className="App">
+    // navigationBar
+    var navigationBar = (
+      <AppBar position='static'>
+        <Toolbar>
+          <Grid justify='space-between' container>
+            <Grid item>
+              <Typography variant='h6'>오늘의 할 일</Typography>
+            </Grid>
+            <Grid item>
+              <Button color='inherit' onClick={signout}>
+                Log out
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    )
+
+    // loading 중이 아닐 때 
+    var todoListPage = (
+      <div>
+        {navigationBar}
         <Container maxWidth="md">
           <AddTodo add={this.add} />
-          <div className="TodoList">{todoItems}</div>
+          <div className='TodoList'>{todoItems}</div>
         </Container>
+      </div>
+    )
+
+    // loading 중일 때 
+    var loadingPage = <h1>로딩중 ...</h1>
+    var content = loadingPage;
+
+    if (!this.state.loading) {
+      content = todoListPage;
+    }
+    
+    // 생성된 컴포넌트 JPX를 리턴한다.
+    return (
+      <div className="App">
+        {content}
       </div>
     );
   }
--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./index.css";
 import App from "./App";
 import Login from "./Login";
-import { BrowerRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 
@@ -19,10 +20,11 @@ function Copyright() {
 class AppRouter extends React.Component {
   render() {
     return (
-      <BrowerRouter>
+      <BrowserRouter>
         <div>
           <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
             <Route path="/" element={<App />} />
           </Routes>
         </div>
@@ -31,7 +33,7 @@ class AppRouter extends React.Component {
             <Copyright />
           </Box>
         </div>
-      </BrowerRouter>
+      </BrowserRouter>
     )
   }
 }
